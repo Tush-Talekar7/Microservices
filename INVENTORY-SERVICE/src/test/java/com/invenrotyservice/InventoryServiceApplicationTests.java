a84920cc64dcb72efd6691e64925432648eeb93f
Tush-Talekar7 <tushartalekar7@gmail.com>
2025-11-02 22:34:54 +0530

    Started working

---
+++ INVENTORY-SERVICE/src/test/java/com/invenrotyservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenrotyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
